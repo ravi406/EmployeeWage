b2a2b60533b30a48bd1cc83d4667e7c23de1d2e4
ravi406 <ravikamble406@gmail.com>
2022-09-03 18:38:49 +0530

    added UC1

---
+++ EmployeeWageDetailsUc1.java
@@ -0,0 +1,16 @@
+package com.dialy.assignments;
+
+public class EmployeeWageDetailsUc1 {
+
+        public static void main(String[] Args) {
+
+            int IS_FULL_TIME = 1;  //CONSTATNT
+
+            double empCheck = Math.floor(Math.random() * 10) % 2;
+
+            if (empCheck == IS_FULL_TIME)
+                System.out.println("Employee is Present");
+            else
+                System.out.println("Employee is Not Present");
+        }
+}
